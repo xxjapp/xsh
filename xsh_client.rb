2841313642575895224529a2c8b96ca55171782f
Xia Xiongjun <xxjapp@gmail.com>
2014-06-14 13:11:20 +0900

    source cleaned up

--- xsh_client.rb
+++ xsh_client.rb
@@ -4,39 +4,36 @@
 require 'socket'
 
 class Client
-  def initialize(server)
-    @server = server
-  end
-
-  def run()
-    request_thread  = send()
-    request_thread.join
-  end
-
-  def send
-    msg = @server.gets.chomp
-    puts msg.force_encoding('UTF-8')
-
-    Thread.new do
-      loop {
-        print "[#{Time.now.strftime('%F %T')}] # "
-        msg = $stdin.gets.chomp
-        next if msg.empty?
-
-        msg.encode!('UTF-8')
-        req_id = Random.rand.to_s
-
-        @server.puts(req_id)
-        @server.puts(msg)
-
-        loop {
-          msg = @server.gets.chomp
-          break if msg == req_id
-          puts msg.force_encoding('UTF-8')
-        }
-      }
+    def initialize(server)
+        @server = server
     end
-  end
+
+    def run()
+        msg = @server.gets.chomp
+        puts msg.force_encoding('UTF-8')
+
+        process_thread = Thread.new do
+            loop {
+                print "[#{Time.now.strftime('%F %T')}] # "
+                msg = $stdin.gets.chomp
+                next if msg.empty?
+
+                msg.encode!('UTF-8')
+                req_id = Random.rand.to_s
+
+                @server.puts(req_id)
+                @server.puts(msg)
+
+                loop {
+                    msg = @server.gets.chomp
+                    break if msg == req_id
+                    puts msg.force_encoding('UTF-8')
+                }
+            }
+        end
+
+        process_thread.join
+    end
 end
 
 server = TCPSocket.open("localhost", 3000)
--- xsh_server.rb
+++ xsh_server.rb
@@ -5,55 +5,55 @@ require 'socket'
 require 'open3'
 
 class Server
-  def initialize(port, ip)
-    @server = TCPServer.open(ip, port)
-  end
+    def initialize(port, ip)
+        @server = TCPServer.open(ip, port)
+    end
 
-  def run()
-    loop {
-      Thread.start(@server.accept) do | client |
-        puts "#{client} connected"
-        client.puts "Connection established!"
-        process(client)
-      end
-    }.join
-  end
+    def run()
+        loop {
+            Thread.start(@server.accept) do | client |
+                puts "#{client} connected"
+                client.puts "Connection established!"
+                process(client)
+            end
+        }.join
+    end
 
-  def process(client)
-    loop {
-      # handle request
-      req_id  = client.gets.chomp
-      request = client.gets.chomp
+    def process(client)
+        loop {
+            # handle request
+            req_id  = client.gets.chomp
+            request = client.gets.chomp
 
-      request.force_encoding('UTF-8').encode!(Encoding.default_external)
+            request.force_encoding('UTF-8').encode!(Encoding.default_external)
 
-      # log request
-      puts "/--------------------------------------------------------------"
-      puts "req_id  : #{req_id}"
-      puts "request : #{request}"
-      puts "--------------------------------"
+            # log request
+            puts "/--------------------------------------------------------------"
+            puts "req_id  : #{req_id}"
+            puts "request : #{request}"
+            puts "--------------------------------"
 
-      # handle response
-      begin
-        response, status = Open3.capture2e("#{request} 2>&1")
-      rescue => e
-        response = e.to_s
-      end
+            # handle response
+            begin
+                response, status = Open3.capture2e("#{request} 2>&1")
+            rescue => e
+                response = e.to_s
+            end
 
-      response.encode!('UTF-8')
+            response.encode!('UTF-8')
 
-      # send response
-      client.puts response
-      client.puts req_id    # used as end of response
+            # send response
+            client.puts response
+            client.puts req_id
 
-      # log response
-      puts response
+            # log response
+            puts response
 
-      puts "--------------------------------"
-      puts "status  : #{status}"
-      puts "\\--------------------------------------------------------------"
-    }
-  end
+            puts "--------------------------------"
+            puts "status  : #{status}"
+            puts "\\--------------------------------------------------------------"
+        }
+    end
 end
 
 Server.new(3000, "localhost").run
